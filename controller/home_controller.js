db03acab7b292f137439edb98a62904583d83a7f
Yasir Khan <yasiralikhan140@gmail.com>
2023-11-11 11:35:34 +0530

    install typescipt

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,15 +1,37 @@
 const Habit = require("../models/habit");
 
-module.exports.home = (req, res) => {
-  Habit.find({}, (err, habits) => {
-    if (err) {
+module.exports.home = async (req, res) => {
+  // try {
+  //   let result = await Habit.find();
+  //   return res.render("home", {
+  //     title: "Habit Tracker",
+  //     habit_list: result,
+  //   });
+  // } catch (err) {
+  //   console.log(`Error in fetching the habits ${err}`);
+  // }
+  Habit.find()
+    .then((result) => {
+      return res.render("home", {
+        title: "Habit Tracker",
+        habit_list: result,
+      });
+    })
+    .catch((err) => {
       console.log(`Error in fetching the habits ${err}`);
-      return;
-    }
-    console.log("fetch successfully");
-    return res.render("home", {
-      title: "Habit Tracker",
-      habit_list: habits,
+      return res.render("home", {
+        title: "Habit Tracker",
+      });
     });
-  });
+  // Habit.find({}, (err, habits) => {
+  //   if (err) {
+  //     console.log(`Error in fetching the habits ${err}`);
+  //     return;
+  //   }
+  //   console.log("fetch successfully");
+  //   return res.render("home", {
+  //     title: "Habit Tracker",
+  //     habit_list: habits,
+  //   });
+  // });
 };
